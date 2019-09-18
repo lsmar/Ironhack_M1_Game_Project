01fee8fe39989d43a621ff2e68efaebcd7a8fb00
LsM Lucas Martins <lucas.smar@gmail.com>
2019-09-18 18:13:17 -0300

    style div responsive by heitor

--- assets/js/levelsObjects.js
+++ assets/js/levelsObjects.js
@@ -1,7 +1,7 @@
 const levelsPredefined = [
   {
     levelDifficulty: "Easy",
-    gridSize: 5,
+    gridSize: 666,
     start: [0, 2],
     end: [4, 4],
     correctPath: [[0, 2], [0, 1], [1, 1], [1, 2], [1, 3], [1, 4], [2, 4], [3, 4], [4, 4]],
@@ -23,6 +23,20 @@ const levelsPredefined = [
     points: {
       difficulty: 5,
       time: [{ time: 1000, points: 15 }, { time: 2000, points: 5 }, { time: Infinity, points: 0 }],
+      gridOn: -5,
+      gridOff: 20
+    }
+  },
+  {
+    levelDifficulty: "Easy",
+    gridSize: 10,
+    start: [2, 2],
+    end: [9, 9],
+    correctPath: [[2, 2], [2, 3], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 9], [5, 9], [6, 9], [7, 9], [8, 9], [9, 9]],
+    amountOfObstacles: 70,
+    points: {
+      difficulty: 20,
+      time: [{ time: 3000, points: 15 }, { time: 6000, points: 5 }, { time: Infinity, points: 0 }],
       gridOn: -5,
       gridOff: 20
     }
