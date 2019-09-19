d60db1c414cec957d37eb993eada230bb7c0b9fd
LsM Lucas Martins <lucas.smar@gmail.com>
2019-09-19 15:57:20 -0300

    teste mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,26 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 var gameControl = {};
+//* Is mobile?
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+)
+  alert("You're using Mobile Device!!");
 
 //* Mouse click event
 canvas.addEventListener("mouseup", e => {
+  if (gameControl.start.isMouseHere(e.clientX, e.clientY)) {
+    gameControl.startMouse(e);
+  }
+});
+//* Mouse click event
+canvas.addEventListener("touchstart", e => {
   if (gameControl.start.isMouseHere(e.clientX, e.clientY)) {
     gameControl.startMouse(e);
   }
